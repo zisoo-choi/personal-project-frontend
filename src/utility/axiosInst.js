b4a46493c7c96b22901747ed1ab5b52d69d9e983
zisoo <zisoo_3@naver.com>
2023-07-09 00:39:08 +0900

    init: [최지수] front CD 세팅 [FCLP-2]

---
+++ src/utility/axiosInst.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import env from "../env";
+
+const axiosInst = axios.create({
+    //baseURL: 'http://localhost:7777',
+    baseURL: env.api.MAIN_API_URL,
+    timeout: 2500
+})
+
+export default axiosInst

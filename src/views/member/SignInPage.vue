db5bd592c100df818cb21822e29074adfc3102bf
zisoo <zisoo_3@naver.com>
2023-07-10 21:24:01 +0900

    feat: [최지수] router 설정하려고 만든 member에 대한 page [FCLP-4]

---
+++ src/views/member/SignInPage.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        여기는 로그인 페이지 입니다.
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/views/member/SignUpPage.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        여기는 회원가입 페이지 입니다.
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>

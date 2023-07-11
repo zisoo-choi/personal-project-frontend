ecd7eb33af7ed0c7474ffa5019e731d840643122
zisoo <zisoo_3@naver.com>
2023-07-11 11:50:59 +0900

    feat: [최지수] Home - Image Carousel [FCLP-3]

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,41 @@
-<template>
-  <hello-world />
+<template lang="">
+  <v-container>
+      <div class="homeImg">
+          <v-carousel :cycle="cycle" :interval="interval" :height="height" v-model="model">
+              <v-carousel-item v-for="(picture, idx) in bannerPictures" :key="idx">
+                  <v-sheet :height="height" tile>
+                      <v-row class="fill-height" align="center" justify="center">
+                          <v-img :src="picture.img" :height="height" contain/>
+                      </v-row>
+                  </v-sheet>
+              </v-carousel-item>
+          </v-carousel>
+      </div>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
   export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
+    data() {
+        return {
+            carouselModel: false,
+            bannerPictures: [
+                { img: require("@/assets/downloadImg/book.png") },
+                { img: require("@/assets/downloadImg/books.png") },
+                { img: require("@/assets/downloadImg/book.png") },
+                { img: require("@/assets/downloadImg/books.png") },
+            ],
+            cycle: true,
+            interval: 3000,
+            height: 400,
+            model: ''
+        }
+    }
   }
 </script>
+
+<style>
+.homeImg {
+  margin-top: 30px;
+}
+</style>
